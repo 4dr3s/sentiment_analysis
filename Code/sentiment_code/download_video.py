20feb9b2134583978d23cd5c50a13ad23858f2a2
4dr3s <andres30092000@hotmail.com>
2024-08-03 11:49:24 -0500

    Recorre todo el json

--- Code/sentiment_code/download_video.py
+++ Code/sentiment_code/download_video.py
@@ -1,19 +1,20 @@
 import yt_dlp
 import pandas as pd
 import json
-file_path = "C:/Users/andre/Downloads/datos_tiktok2.json"
+file_path = "C:/Users/andre/Downloads/datos_tiktok_threads_instagram_twitter.json"
 with open(file_path, 'r', encoding='utf-8') as f:
     data = json.load(f)
 df = pd.json_normalize(data)
 
 for i, item in df.iterrows():
     try:
-        ydl_opts = {
-            'format': 'mp4',
-            'outtmpl': f"C:/Universidad/Legislacion/videos/{item['id']}.mp4",
-        }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([f"{item['post_link']}"])
+        if item['fuente'] == 'Tik Tok':
+            ydl_opts = {
+                'format': 'mp4',
+                'outtmpl': f"C:/Universidad/Legislacion/videos/{item['id']}.mp4",
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([f"{item['post_link']}"])
     except Exception as ex:
         print(ex)
         continue
