8dc077eb9fe3dc7fe7c802c4e67ccac03ff72708
4dr3s <andres30092000@hotmail.com>
2024-08-03 21:42:20 -0500

    Recorre todo el json

--- Code/sentiment_code/download_video.py
+++ Code/sentiment_code/download_video.py
@@ -9,12 +9,13 @@ df = pd.json_normalize(data)
 for i, item in df.iterrows():
     try:
         if item['fuente'] == 'Tik Tok':
-            ydl_opts = {
-                'format': 'mp4',
-                'outtmpl': f"C:/Universidad/Legislacion/videos/{item['id']}.mp4",
-            }
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([f"{item['post_link']}"])
+            if 36 <= item['id'] <= 100:
+                ydl_opts = {
+                    'format': 'mp4',
+                    'outtmpl': f"C:/Universidad/Legislacion/videos/{item['id']}.mp4",
+                }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([f"{item['post_link']}"])
     except Exception as ex:
         print(ex)
         continue
